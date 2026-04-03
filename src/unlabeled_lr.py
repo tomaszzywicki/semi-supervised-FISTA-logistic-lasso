58074298dc7f659eedcc5448d66d96135c4cc134
krukj <126516292+krukj@users.noreply.github.com>
2026-04-03 15:08:18 +0200

    add choosing min k for label prop and imputation performance history

--- src/unlabeled_lr.py
+++ src/unlabeled_lr.py
@@ -4,7 +4,9 @@ from typing import Literal
 
 import numpy as np
 from numpy.typing import ArrayLike
+from scipy.sparse.csgraph import connected_components
 from sklearn.base import BaseEstimator, clone
+from sklearn.metrics import balanced_accuracy_score
 from sklearn.neighbors import kneighbors_graph
 
 from fista import LogisticLassoFistaCV
@@ -28,6 +30,7 @@ class UnlabeledLogReg:
             | None
         ) = "random",
         k_best: int = 1,
+        sigma: float = 10,
         base_estimator: BaseEstimator | None = None,
     ) -> None:
         """
@@ -42,25 +45,38 @@ class UnlabeledLogReg:
                 - 'pseudo_labels'
                 - 'self_training'
                 - 'label_propagation'
+            k_best (int): Number of biggest probabilties to be selected in self_training method.
+                Defaults to 1.
+            sigma (float): Sigma paramater for the distance function in label_propagation method.
+                Deafults to 1.0.
         """
         self.y_imputation_method = y_imputation_method
         self.model = LogisticLassoFistaCV()
         self.k_best = k_best
+        self.sigma = sigma
         self.base_estimator = base_estimator
 
-    def fit(self, X: ArrayLike, y_obs: ArrayLike) -> "UnlabeledLogReg":
+    def fit(
+        self, X: ArrayLike, y_obs: ArrayLike, y_true: ArrayLike | None = None
+    ) -> "UnlabeledLogReg":
         """
         Fit the model according to the given training data.
 
         Args:
             X (ArrayLike): Training matrix of size (n_samples, n_features).
             y_obs (ArrayLike): Target vector with potential missing values, relative to X.
+            y_true (ArrayLike | None, optional): Target vector without missing values.
+                Used to check the performance of label imputation. Defaults to None
 
         Returns:
             UnlabeledLogReg: Fitted estimator.
         """
         X = np.asarray(X)
         y_obs = np.asarray(y_obs)
+        if y_true is not None:
+            y_true = np.asarray(y_true)
+            self.y_true = y_true
+        self.imputation_scores = []
 
         X_complete, y_complete = self._impute(X, y_obs)  # Subset with not missing y
 
@@ -73,11 +89,20 @@ class UnlabeledLogReg:
         }
 
         if self.y_imputation_method in _imputation_methods:
-            _imputation_methods[self.y_imputation_method](X, y_obs, X_complete, y_complete)
+            _imputation_methods[self.y_imputation_method](
+                X, y_obs, X_complete, y_complete
+            )
 
         return self
 
-    def _fit_pseudo_labels(self, X, y_obs, X_complete, y_complete) -> None:
+    def _fit_pseudo_labels(
+        self,
+        X: ArrayLike,
+        y_obs: ArrayLike,
+        X_complete: ArrayLike,
+        y_complete: ArrayLike,
+    ) -> None:
+        """Fit the model with a pseudo labels approach."""
         # train model on data without missing labels, predict them and train again
         y_pred = self.model.predict(self.X_original)
 
@@ -88,7 +113,14 @@ class UnlabeledLogReg:
 
         self.model.fit(self.X_original, y_all)
 
-    def _fit_self_training(self, X, y_obs, X_complete, y_complete) -> None:
+    def _fit_self_training(
+        self,
+        X: ArrayLike,
+        y_obs: ArrayLike,
+        X_complete: ArrayLike,
+        y_complete: ArrayLike,
+    ) -> None:
+        """Fit the model with a self training approach."""
         # Base estimator
         if self.base_estimator is None:
             clf = clone(self.model)
@@ -103,6 +135,9 @@ class UnlabeledLogReg:
         X_missing = X[y_obs == -1]
 
         k = self.k_best
+
+        missing_indices = np.where(y_obs == -1)[0]
+        y_imputed = np.full(len(y_obs), -1)
 
         while len(X_missing) > 0:
             clf.fit(X_train, y_train)
@@ -124,16 +159,43 @@ class UnlabeledLogReg:
             y_train = np.concatenate([y_train, np.argmax(y_prob[idx_to_add], axis=1)])
             X_missing = np.delete(X_missing, idx_to_add, axis=0)
 
+            original_indices = missing_indices[idx_to_add]
+            y_imputed[original_indices] = np.argmax(y_prob[idx_to_add], axis=1)
+
+            missing_indices = np.delete(missing_indices, idx_to_add)
+
+            self._calculate_imputation_performance(y_obs, y_imputed)
+
         self.model.fit(X_train, y_train)
 
-    def _fit_label_propagation(self, X, y_obs, X_complete, y_complete) -> None:
+    def _fit_label_propagation(
+        self,
+        X: ArrayLike,
+        y_obs: ArrayLike,
+        X_complete: ArrayLike,
+        y_complete: ArrayLike,
+    ) -> None:
+        """Fit the model with a label propagation approach."""
+
         # knn graph with distances between points
-        W = kneighbors_graph(X, n_neighbors=5, mode="distance").toarray()
+        def find_minimal_k(X: ArrayLike):
+            "Find minimal k for which the kNN graph is connected."
+            k = 1
+            while True:
+                W = kneighbors_graph(X, n_neighbors=k, mode="distance").toarray()
+                n_components, _ = connected_components(W)
+                if n_components == 1:
+                    break
+                k += 1
+            return k
+
+        k = find_minimal_k(X)
+        W = kneighbors_graph(X, n_neighbors=k, mode="distance").toarray()
+        W = np.maximum(W, W.T)
         zero_mask = W == 0
 
         # gaussian kernel (distances -> similarities)
-        sigma = 1.0
-        W = np.exp((-(W**2)) / sigma**2)
+        W = np.exp((-(W**2)) / self.sigma**2)
         W[zero_mask] = 0
 
         # normalizing to create T (T = probabilities)
@@ -152,15 +214,29 @@ class UnlabeledLogReg:
             labeled_mask = self.y_original != -1
             F[labeled_mask] = Y[labeled_mask]  # original labels
             # if changes between iterations are small
-            if np.max(np.abs(F - F_prev)) < 1e-5:
+            if np.max(np.abs(F - F_prev)) < 1e-3:
                 break
 
+            self._calculate_imputation_performance(y_obs, (F >= 0.5).astype(int))
             F_prev = F.copy()
 
-        y_completed = F.copy()
+        y_completed = (F >= 0.5).astype(int)
         self.model.fit(self.X_original, y_completed)
 
-        return self
+    def _calculate_imputation_performance(
+        self, y_obs: ArrayLike, y_imput_curr: ArrayLike
+    ) -> None:
+        """Calculate balanced accuracy of iterative imputation method."""
+        if not hasattr(self, "y_true"):
+            return
+        missing_mask = y_obs == -1
+        y_true_missing = self.y_true[missing_mask]
+        y_imput_missing = y_imput_curr[missing_mask]
+        valid_mask = y_imput_missing != -1
+        score = balanced_accuracy_score(
+            y_true_missing[valid_mask], y_imput_missing[valid_mask]
+        )
+        self.imputation_scores.append(score)
 
     def validate(
         self, X: ArrayLike, y: ArrayLike, measure: str, prob_threshold: float = 0.5
@@ -246,7 +322,9 @@ class UnlabeledLogReg:
         y[missing_mask] = np.random.binomial(n=1, p=0.5, size=missing_mask.sum())
         return y
 
-    def _naive_imputation(self, X: ArrayLike, y: ArrayLike) -> tuple[ArrayLike, ArrayLike]:
+    def _naive_imputation(
+        self, X: ArrayLike, y: ArrayLike
+    ) -> tuple[ArrayLike, ArrayLike]:
         """
         Remove records with missing y.
 
@@ -260,7 +338,9 @@ class UnlabeledLogReg:
         missing_mask = y == -1
         return X[~missing_mask], y[~missing_mask]
 
-    def _pseudo_labeling(self, X: ArrayLike, y: ArrayLike) -> tuple[ArrayLike, ArrayLike]:
+    def _pseudo_labeling(
+        self, X: ArrayLike, y: ArrayLike
+    ) -> tuple[ArrayLike, ArrayLike]:
         """
         Remove records with missing y, but save the original X and y.
 
